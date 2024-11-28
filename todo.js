ccf2e1201f73a8345c3ef3d9e6b9ca9aa984f278
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-11-28 13:36:49 +0530

    Todolist

---
+++ todo.js
@@ -0,0 +1,10 @@
+const inputDatas = document.getElementById("inputData");
+
+document.getElementById("btnData").addEventListener("click", function(){
+    const li = document.createElement("li");
+    const editBtn = document.createElement("button");
+    const deleteBtn = document.createElement("button");
+
+    
+
+})
