e56f01d2ac85be79113feab940af6cb7623abc1c
rgmanikandan07 <rgmanikandan07@gmail.com>
2024-11-28 14:49:57 +0530

    Todolist

--- todo.js
+++ todo.js
@@ -1,10 +1,27 @@
-const inputDatas = document.getElementById("inputData");
-
 document.getElementById("btnData").addEventListener("click", function(){
-    const li = document.createElement("li");
-    const editBtn = document.createElement("button");
-    const deleteBtn = document.createElement("button");
+    const inputDatas = document.getElementById("inputData");
+    const taskText = inputDatas.value.trim();  
 
-    
+    if(taskText !== ""){
+        const li = document.createElement("li");
+        const deleteBtn = document.createElement("button");
+
+        li.innerHTML = taskText;
+        deleteBtn.innerHTML = "Remove";
+
+        deleteBtn.addEventListener("click", function(){
+            li.remove();
+        })
+
+        li.appendChild(deleteBtn);
+
+        const unOrderlist = document.getElementById("taskList");
+        unOrderlist.appendChild(li);
+
+        inputDatas.value = "";
+
+    } else{
+        alert("Please Enter the Task");
+    }
 
 })
